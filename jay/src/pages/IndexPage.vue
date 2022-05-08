66a8dd4501db7485ef6f90b8d3d4da757613b176
Jay <jayceon310893@gmail.com>
2022-05-08 15:39:42 +0300

    jaylink

---
+++ jay/src/pages/IndexPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-page class="flex flex-direction: column flex-center">
+    <q-avatar size="130px">
+        <img src="~assets/avatar.jpg">
+      </q-avatar>
+    <img
+      alt="KIKA PRO MAKEUP"
+      src="~assets/KIKAPROMAKEUP.svg"
+      style="width: 200px; height: 200px"
+    >
+  </q-page>
+</template>
+
+
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
---
+++ jay/src/router/routes.js
@@ -0,0 +1,22 @@
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/MenuLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: '/photo', component: () => import('pages/PhotoPage.vue') },
+      { path: '/video', component: () => import('pages/VideoPage.vue') },
+      { path: '/record', component: () => import('pages/RecordPage.vue') },
+    ]
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/ErrorNotFound.vue')
+  }
+]
+
+export default routes
