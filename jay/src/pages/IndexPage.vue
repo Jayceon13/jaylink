752821d5a1469d225e161d602e456d0082d6ddc0
Jay <jayceon310893@gmail.com>
2022-05-08 23:25:06 +0300

    btns

--- jay/src/pages/IndexPage.vue
+++ jay/src/pages/IndexPage.vue
@@ -1,17 +1,23 @@
 <template>
-  <q-page class="flex flex-direction: column flex-center">
-    <q-avatar size="130px">
-        <img src="~assets/avatar.jpg">
-      </q-avatar>
-    <img
-      alt="KIKA PRO MAKEUP"
-      src="~assets/KIKAPROMAKEUP.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+  <div class="list-page">
+    <q-page class="flex flex-direction: column flex-center">
+      <q-avatar size="130px">
+          <img src="~assets/avatar.jpg">
+        </q-avatar>
+      <img
+        alt="KIKA PRO MAKEUP"
+        src="~assets/KIKAPROMAKEUP.svg"
+        style="width: 200px; height: 200px"
+      >
+    </q-page>
+  </div>
 </template>
 
-
+<style lang="scss">
+.list-page {
+  background-image: linear-gradient(#FBEEFF, #D7A6E8);
+}
+</style>
 
 <script>
 import { defineComponent } from 'vue'
